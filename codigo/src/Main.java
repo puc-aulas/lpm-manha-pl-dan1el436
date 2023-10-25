deb044a65a99ed365adf1658360adb87bdb83926
Lucas Flor Vilela <lucasvilelapessoal@gmail.com>
2023-10-25 10:18:06 -0300

    Update Main.java

--- codigo/src/Main.java
+++ codigo/src/Main.java
@@ -29,8 +29,8 @@ public class Main{
 		biblioteca.adicionarDvd("A Pequena Sereia","Disney","Dvd","2023",3,3,5,3,"Aventura");
 		
 		biblioteca.adicionarLivro("Dracula","Bram Stoker","Livro","2023",3,3,5,"Terror","A presença profana de Drácula condena a tripulação do navio mercante Demeter.");
-		biblioteca.adicionarLivro("O Senhor dos Aneis","J. R. R. Tolkien","Tese","2023",3,3,5,"Aventura","O Senhor dos Anéis é um livro de alta fantasia, escrito pelo escritor britânico J. R. R. Tolkien. Escrita entre 1937 e 1949, com muitas partes criadas durante a Segunda Guerra Mundial, a saga é uma continuação de O Hobbit.");
-		biblioteca.adicionarLivro("Como treinar seu dragao","Cressida Cowell","Tese","2023",3,3,5,"Aventura","How to Train Your Dragon é uma série de livros infantojuvenil criados pela escritora Cressida Cowell e lançado pela editora Holden Children's Books em 27 de junho de 2003 no Reino Unido. A série é dividida em treze livros, com onze lançados, e dois extras. No Brasil, a série foi lançada pela Editora Intrínseca.");
+		biblioteca.adicionarLivro("O Senhor dos Aneis","J. R. R. Tolkien","Livro","2023",3,3,5,"Aventura","O Senhor dos Anéis é um livro de alta fantasia, escrito pelo escritor britânico J. R. R. Tolkien. Escrita entre 1937 e 1949, com muitas partes criadas durante a Segunda Guerra Mundial, a saga é uma continuação de O Hobbit.");
+		biblioteca.adicionarLivro("Como treinar seu dragao","Cressida Cowell","Livro","2023",3,3,5,"Aventura","How to Train Your Dragon é uma série de livros infantojuvenil criados pela escritora Cressida Cowell e lançado pela editora Holden Children's Books em 27 de junho de 2003 no Reino Unido. A série é dividida em treze livros, com onze lançados, e dois extras. No Brasil, a série foi lançada pela Editora Intrínseca.");
 		
 		Scanner scan = new Scanner(System.in);
 		int key = 0;
